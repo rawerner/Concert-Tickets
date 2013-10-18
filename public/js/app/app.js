244872d132b19a08ed00bff52043f0ca33c28c01
Rachel Werner <rachelannwerner@gmail.com>
2013-10-18 13:36:36 -0500

    Building Table functions

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -1,68 +1,102 @@
-// 'use strict';
+'use strict';
 
-// // Firebase Schema
-// var Δdb;
+// Firebase Schema
 
-// // Local Schema (defined in keys.js)
 
-// $(document).ready(initialize);
+// Local Schema (defined in keys.js)
 
-// function initialize(fn, flag){
-//   if(!canRun(flag)) {return;}
+$(document).ready(initialize);
 
-//   $(document).foundation();
-//   Δdb = new Firebase(db.keys.firebase);
-//   initMap(36, -86, 5);
-// }
+function initialize(fn, flag){
+  if(!canRun(flag)) {return;}
 
-// // -------------------------------------------------------------------- //
-// // -------------------------------------------------------------------- //
-// // -------------------------------------------------------------------- //
+  $(document).foundation();
+  $('#createSection').click(clickCreateSection);
 
-// function initMap(lat, lng, zoom){
-//   var mapOptions = {center: new google.maps.LatLng(lat, lng), zoom: zoom, mapTypeId: google.maps.MapTypeId.ROADMAP};
-//   db.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-// }
+}
 
-// // -------------------------------------------------------------------- //
-// // -------------------------------------------------------------------- //
-// // -------------------------------------------------------------------- //
 
-// function getValue(selector, fn){
-//   var value = $(selector).val();
-//   value = value.trim();
-//   $(selector).val('');
+// -------------------------------------------------------------------- //
+// -------------------------------------------------------------------- //
+// -------------------------------------------------------------------- //
+function clickCreateSection(){
+  var section = $('select').val();
+  var numOfSeats = $('#numOfSeats').val();
+  var priceOfSeats = $('#priceOfSeats').val();
 
-//   if(fn){
-//     value = fn(value);
-//   }
+  htmlCreateTable(numOfSeats);
 
-//   return value;
-// }
+}
 
-// function parseUpperCase(string){
-//   return string.toUpperCase();
-// }
 
-// function parseLowerCase(string){
-//   return string.toLowerCase();
-// }
+// -------------------------------------------------------------------- //
+// -------------------------------------------------------------------- //
+// -------------------------------------------------------------------- //
 
-// function formatCurrency(number){
-//   return '$' + number.toFixed(2);
-// }
+function htmlCreateTable(numOfSeats){
+  var $table = $('<table>');
+  $('#gasection').append($table);
+  var numberOfFullRows = numOfSeats / 7;
+  numberOfFullRows = Math.floor(numberOfFullRows);
+  var seatsInLastRow = numOfSeats % 7;
 
-// // -------------------------------------------------------------------- //
-// // -------------------------------------------------------------------- //
-// // -------------------------------------------------------------------- //
+  htmlCreateFullRows(numberOfFullRows);
+  htmlCreateLastRow(seatsInLastRow);
+}
 
-// function canRun(flag){
-//   var isQunit = $('#qunit').length > 0;
-//   var isFlag = flag !== undefined;
-//   var value = isQunit && isFlag || !isQunit;
-//   return value;
-// }
+function htmlCreateFullRows(numberOfFullRows){
+  var $tr = $('<tr>');
+  $('#')
 
-// // -------------------------------------------------------------------- //
-// // -------------------------------------------------------------------- //
-// // -------------------------------------------------------------------- //
+}
+
+function htmlCreateLastRow(seatsInLastRow){
+  var $tr = $('<tr>');
+  $('#')
+
+}
+
+
+
+//-------------------------------------------------------------------- //
+// -------------------------------------------------------------------- //
+// -------------------------------------------------------------------- //
+
+function getValue(selector, fn){
+  var value = $(selector).val();
+  value = value.trim();
+  $(selector).val('');
+
+  if(fn){
+    value = fn(value);
+  }
+
+  return value;
+}
+
+function parseUpperCase(string){
+  return string.toUpperCase();
+}
+
+function parseLowerCase(string){
+  return string.toLowerCase();
+}
+
+function formatCurrency(number){
+  return '$' + number.toFixed(2);
+}
+
+// -------------------------------------------------------------------- //
+// -------------------------------------------------------------------- //
+// -------------------------------------------------------------------- //
+
+function canRun(flag){
+  var isQunit = $('#qunit').length > 0;
+  var isFlag = flag !== undefined;
+  var value = isQunit && isFlag || !isQunit;
+  return value;
+}
+
+// -------------------------------------------------------------------- //
+// -------------------------------------------------------------------- //
+// -------------------------------------------------------------------- //
--- public/js/tests/master.js
+++ public/js/tests/master.js
@@ -1,14 +1,24 @@
-// 'use strict';
+'use strict';
 
-// module('Integration Testing', {setup: setupTest, teardown: teardownTest});
+module('Integration Testing', {setup: setupTest, teardown: teardownTest});
 
-// function setupTest(){
-//   initialize(null, true);
-// }
+function setupTest(){
+  initialize(null, true);
+}
 
-// function teardownTest(){
-// }
+function teardownTest(){
+}
 
-// test('<name-of-feature>', function(){
-//   expect(1);
-// });
+test('section Creation', function(){
+  expect(4);
+
+  $('#select').val('ga');
+  $('#numOfSeats').val('13');
+  $('#priceofSeats').val('100');
+  $('#createSection').trigger('click');
+
+  deepEqual($('#gasection > table').length, 1, 'Should have created a table');
+  deepEqual($('#gasection > table > tr').length, 2, 'Should have created a table row');
+  deepEqual($('#gasection > table > tr:nth-child(1) > td').length, 7, 'Should have created 7 columns in row 1');
+  deepEqual($('#gasection > table > tr:nth-child(2) > td').length, 6, 'Should have created 6 columns in row 2');
+});
