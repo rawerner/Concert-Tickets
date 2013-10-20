ed89ce4c8d28deaa3d0c40993f5fecd9018f65df
Rachel Werner <rachelannwerner@gmail.com>
2013-10-20 07:48:09 -0500

    updated seats in row to 14 and changed css

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -12,6 +12,8 @@ function initialize(fn, flag){
 
   $(document).foundation();
   $('#createSection').click(clickCreateSection);
+  $('#gasection').on('dblclick', '.seat', clickAddName);
+  $('#vipsection').on('dblclick', '.seat', clickAddName);
 
 }
 
@@ -23,11 +25,30 @@ function clickCreateSection(){
   var section = $('select').val();
   var numOfSeats = $('#numOfSeats').val();
   var priceOfSeats = $('#priceOfSeats').val();
+  var option = section.slice(1);
+  var optionClass = '.' + option;
+  var $option = $(optionClass);
 
   htmlCreateTable(numOfSeats);
-
+  htmlRemoveOption($option);
+  if ($('select > option').length === 0){
+    htmlRemoveSectionCreation();
+  }
 }
 
+
+function clickAddName(){
+  var $this = $(this);
+  //if (this td doesnt already has a span inside it){
+  var name = $('#name').val();
+  var $span = $('<span>');
+  $span.text(name);
+
+  $this.addClass('reserved');
+  $this.append($span);
+  //}
+
+}
 
 // -------------------------------------------------------------------- //
 // -------------------------------------------------------------------- //
@@ -35,29 +56,65 @@ function clickCreateSection(){
 
 function htmlCreateTable(numOfSeats){
   var $table = $('<table>');
-  $('#gasection').append($table);
-  var numberOfFullRows = numOfSeats / 7;
+  var section = $('select').val();
+  $table.addClass(section);
+  $(section).append($table);
+  var numberOfFullRows = numOfSeats / 14;
   numberOfFullRows = Math.floor(numberOfFullRows);
-  var seatsInLastRow = numOfSeats % 7;
+  var seatsInLastRow = numOfSeats % 14;
 
-  htmlCreateFullRows(numberOfFullRows);
-  htmlCreateLastRow(seatsInLastRow);
+  htmlCreateFullRows(numberOfFullRows, $table);
+  if (seatsInLastRow){
+    htmlCreateLastRow(seatsInLastRow, $table);
+  }
 }
 
-function htmlCreateFullRows(numberOfFullRows){
+function htmlCreateFullRows(numberOfFullRows, $table){
+
+  for(var i = 0; i < numberOfFullRows; i++){
+    var $tr = $('<tr>');
+    var $td1 = $('<td>');
+    var $td2 = $('<td>');
+    var $td3 = $('<td>');
+    var $td4 = $('<td>');
+    var $td5 = $('<td>');
+    var $td6 = $('<td>');
+    var $td7 = $('<td>');
+    var $td8 = $('<td>');
+    var $td9 = $('<td>');
+    var $td10 = $('<td>');
+    var $td11 = $('<td>');
+    var $td12 = $('<td>');
+    var $td13 = $('<td>');
+    var $td14 = $('<td>');
+    $tr.append($td1, $td2, $td3, $td4, $td5, $td6, $td7, $td8, $td9, $td10, $td11, $td12, $td13, $td14 );
+    $tr.children('td').addClass('seat');
+    $table.append($tr);
+  }
+
+}
+
+function htmlCreateLastRow(seatsInLastRow, $table){
   var $tr = $('<tr>');
-  $('#')
+
+  for(var i = 0; i < seatsInLastRow; i++){
+    var $td = $('<td>');
+    $td.addClass('seat');
+    $tr.append($td);
+  }
+  $table.append($tr);
+}
+
+
+function htmlRemoveOption($option){
+  $option.remove();
 
 }
 
-function htmlCreateLastRow(seatsInLastRow){
-  var $tr = $('<tr>');
-  $('#')
+function htmlRemoveSectionCreation(){
+  $('select').parent().parent().remove();
 
 }
-
-
-
 //-------------------------------------------------------------------- //
 // -------------------------------------------------------------------- //
 // -------------------------------------------------------------------- //
--- public/js/tests/master.js
+++ public/js/tests/master.js
@@ -18,7 +18,33 @@ test('section Creation', function(){
   $('#createSection').trigger('click');
 
   deepEqual($('#gasection > table').length, 1, 'Should have created a table');
-  deepEqual($('#gasection > table > tr').length, 2, 'Should have created a table row');
-  deepEqual($('#gasection > table > tr:nth-child(1) > td').length, 7, 'Should have created 7 columns in row 1');
-  deepEqual($('#gasection > table > tr:nth-child(2) > td').length, 6, 'Should have created 6 columns in row 2');
+  deepEqual($('#gasection > table > tbody > tr').length, 2, 'Should have created a table row');
+  deepEqual($('#gasection > table > tbody > tr:nth-child(1) > td').length, 7, 'Should have created 7 columns in row 1');
+  deepEqual($('#gasection > table > tbody > tr:nth-child(2) > td').length, 6, 'Should have created 6 columns in row 2');
+});
+
+test('removing option', function(){
+  expect(1);
+  $('#select').val('ga');
+  $('#numOfSeats').val('13');
+  $('#priceofSeats').val('100');
+  $('#createSection').trigger('click');
+
+  deepEqual($('select > option').length, 1, 'Should have only one section left');
+
+});
+
+test('double click adds name', function(){
+  expect(1);
+  $('#select').val('ga');
+  $('#numOfSeats').val('13');
+  $('#priceofSeats').val('100');
+  $('#createSection').trigger('click');
+
+  $('#name').val('Rachel');
+  $('#gasection > table > tbody > tr:first-child > td:first-child').trigger('dblclick');
+
+
+  deepEqual($('#gasection > table > tbody > tr:first-child > td:first-child > span').text(), 'Rachel', 'Should have Rachel inside td');
+
 });
