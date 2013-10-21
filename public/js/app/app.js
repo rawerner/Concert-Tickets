c5101c96064118b7853c3a533488b7929be6110a
BenjamenB <benjamen.brescka@gmail.com>
2013-10-21 08:54:50 -0500

    This should work :P

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -3,7 +3,20 @@
 // Firebase Schema
 
 
-// Local Schema (defined in keys.js)
+// Local Schema
+var venue={};
+// Following are not needed, only explicitly put out for visualization purposes
+venue.gasection={};
+venue.gasection.price=0;
+venue.gasection.tickets=0;
+venue.gasection.gross=0;
+venue.vipsection={};
+venue.vipsection.price=0;
+venue.vipsection.tickets=0;
+venue.vipsection.gross=0;
+
+
+
 
 $(document).ready(initialize);
 
@@ -14,7 +27,6 @@ function initialize(fn, flag){
   $('#createSection').click(clickCreateSection);
   $('#gasection').on('dblclick', '.seat', clickAddName);
   $('#vipsection').on('dblclick', '.seat', clickAddName);
-
 }
 
 
@@ -24,32 +36,47 @@ function initialize(fn, flag){
 function clickCreateSection(){
   var section = $('select').val();
   var numOfSeats = $('#numOfSeats').val();
-  var priceOfSeats = $('#priceOfSeats').val();
+  var priceOfSeats = parseInt($('#priceOfSeats').val(),10);
   var option = section.slice(1);
+  venue[option].price = priceOfSeats;
+  var reportPriceClass = '.' + option + 'price';
+  $(reportPriceClass).text('$' + priceOfSeats);
   var optionClass = '.' + option;
   var $option = $(optionClass);
 
   htmlCreateTable(numOfSeats);
   htmlRemoveOption($option);
+  $('#numOfSeats').val('');
+  $('#numOfSeats').focus();
+  $('#priceOfSeats').val('');
   if ($('select > option').length === 0){
     htmlRemoveSectionCreation();
-    // htmlAddReportSection;
     $('.hidden').removeClass('hidden');
+    $('.title').addClass('hidden');
   }
 }
 
 
 function clickAddName(){
   var $this = $(this);
-  //if (this td doesnt already has a span inside it){
-  var name = $('#name').val();
-  var $span = $('<p>');
-  $span.text(name);
-
-  $this.addClass('reserved');
-  $this.append($span);
-  //}
-
+  var seatNumber = $this.text();
+  if ($this.hasClass('reserved') === false){
+    var name = $('#name').val();
+    var $span = $('<p>');
+    $span.text(name);
+    $this.addClass('reserved');
+    if ($this.parent().parent().parent().parent().attr('id') === 'gasection'){
+      htmlAddGuest(name, '#galist', seatNumber);
+      htmlSectionTotalUpdate('gasection');
+    } else {
+      htmlAddGuest(name, '#viplist', seatNumber);
+      htmlSectionTotalUpdate('vipsection');
+    }
+    htmlGrandTotalUpdate();
+    $this.append($span);
+  }
+  $('#name').focus();
+  $('#name').val('');
 }
 
 // -------------------------------------------------------------------- //
@@ -64,15 +91,12 @@ function htmlCreateTable(numOfSeats){
   var numberOfFullRows = numOfSeats / 14;
   numberOfFullRows = Math.floor(numberOfFullRows);
   var seatsInLastRow = numOfSeats % 14;
-  var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-  htmlCreateFullRows(alphabet, numberOfFullRows, $table);
-  if (seatsInLastRow){
-    htmlCreateLastRow(alphabet, seatsInLastRow, $table, numberOfFullRows);
-  }
+  htmlCreateRows(numberOfFullRows, $table, seatsInLastRow);
 }
 
-function htmlCreateFullRows(alphabet, numberOfFullRows, $table){
+function htmlCreateRows(numberOfFullRows, $table, seatsInLastRow){
   var alphabetCounter = 0;
+  var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   for(var i = 0; i < numberOfFullRows; i++){
     var $tr = $('<tr>');
     for (var j = 0; j < 14; j++){
@@ -84,64 +108,48 @@ function htmlCreateFullRows(alphabet, numberOfFullRows, $table){
     alphabetCounter++;
     $table.append($tr);
   }
-}
 
-function htmlCreateLastRow(alphabet, seatsInLastRow, $table, numberOfFullRows){
-  var $tr = $('<tr>');
-
-  for(var i = 0; i < seatsInLastRow; i++){
-    var $td = $('<td>');
-    $td.addClass('seat');
-    $td.text(alphabet[numberOfFullRows] + (i+1));
-    $tr.append($td);
+  if (seatsInLastRow) {
+    var $tr = $('<tr>');
+    for(var k = 0; k < seatsInLastRow; k++){
+      var $td = $('<td>');
+      $td.addClass('seat');
+      $td.text(alphabet[numberOfFullRows] + (k+1));
+      $tr.append($td);
+    }
+    $table.append($tr);
   }
-  $table.append($tr);
 }
-
 
 function htmlRemoveOption($option){
   $option.remove();
-
 }
 
 function htmlRemoveSectionCreation(){
   $('select').parent().parent().remove();
-
 }
 
-// function htmlAddReportSection(){
-//   var $div = $('<div>');
-//   $div.addClass('row');
-//   $('.firstrow').after($div);
-// }
-//-------------------------------------------------------------------- //
-// -------------------------------------------------------------------- //
-// -------------------------------------------------------------------- //
-
-function getValue(selector, fn){
-  var value = $(selector).val();
-  value = value.trim();
-  $(selector).val('');
-
-  if(fn){
-    value = fn(value);
-  }
-
-  return value;
+function htmlAddGuest(name, list, seatNumber) {
+  var $li = $('<li>');
+  $li.text(name + ' - ' + seatNumber);
+  $(list).append($li);
 }
 
-function parseUpperCase(string){
-  return string.toUpperCase();
+function htmlSectionTotalUpdate(section){
+  venue[section].tickets++;
+  var ticketTotal =  venue[section].tickets;
+  $('.' + section + 'tickets').text(ticketTotal);
+  venue[section].gross = ticketTotal * venue[section].price;
+  var sectionGross = venue[section].gross;
+  $('.' + section + 'gross').text('$' + sectionGross);
 }
 
-function parseLowerCase(string){
-  return string.toLowerCase();
+function htmlGrandTotalUpdate(){
+  var totalTickets = venue.gasection.tickets + venue.vipsection.tickets;
+  $('#grandtickets').val(totalTickets);
+  var totalGross = parseInt(venue.gasection.gross, 10) + parseInt(venue.vipsection.gross,10);
+  $('#grandgross').val('$' + totalGross);
 }
-
-function formatCurrency(number){
-  return '$' + number.toFixed(2);
-}
-
 // -------------------------------------------------------------------- //
 // -------------------------------------------------------------------- //
 // -------------------------------------------------------------------- //
